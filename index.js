fc9eb0bfb17c0eb38c108f7c2910e1bc13c01c1c
Em Wilson <innotis@gmail.com>
2026-07-26 17:14:09 -0400

    Adjust default zoom level

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { BrowserMultiFormatReader, BrowserMultiFormatOneDReader } from '@zxing/b
 import { BarcodeFormat, DecodeHintType } from '@zxing/library';
 ;
 
-const defaultZoom = 3;
+const defaultZoom = 2.5;
 
 const els = {
       addBtn: document.querySelector("#addBtn"),
